11960047e868176b872a4999a5a178917dce78e3
OvanessTea <OvanessTea@yandex.com>
2025-07-22 23:11:00 +0200

    add entrypoint, configs

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <h1>
+          Hello, MetaMask DApp!
+        </h1>
+        <p>
+          Your React application is now running successfully!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
